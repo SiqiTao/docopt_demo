6a3ccca1500c28cf1ca0386455863ac5c0714ce3
Siqi Tao <taosiqi728@163.com>
2021-11-21 16:13:39 -0800

    Finish Q6

--- demo.R
+++ demo.R
@@ -14,6 +14,10 @@ Options:
 library(docopt)
 opt <- docopt(doc)
 
-print(opt)
-print(typeof(opt))
-print(opt$arg4)
+main <- function(arg1, arg2, arg3, arg4) {
+    print(opt)
+    print(typeof(opt))
+    print(opt$arg4)
+}
+
+main(opt$arg1, opt$arg2, opt$arg3, opt$arg4)
